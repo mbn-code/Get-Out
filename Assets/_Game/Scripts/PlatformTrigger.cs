293d4d115205c0c8d491a182dfdf14e4bdbd8b15
mbn <66748817+mbn-code@users.noreply.github.com>
2025-01-06 19:50:08 +0100

    Platform trigger working

--- Assets/_Game/Scripts/PlatformTrigger.cs
+++ Assets/_Game/Scripts/PlatformTrigger.cs
@@ -2,30 +2,27 @@ using UnityEngine;
 
 public class PlatformTrigger : MonoBehaviour
 {
-    [SerializeField] private string correctSpriteName; // Name of the correct sprite
+    [SerializeField] private string correctObjectName; // Name of the correct object
     [SerializeField] private GameObject actionObject; // Object to activate or any other action
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter(Collider other)
     {
-        // Check if the collided object has a SpriteRenderer
-        SpriteRenderer spriteRenderer = collision.GetComponent<SpriteRenderer>();
-        if (spriteRenderer != null)
+        // Check if the collided object has the correct name
+        if (other.gameObject.name == correctObjectName)
         {
-            // Check if the sprite name matches the correct sprite name
-            if (spriteRenderer.sprite.name == correctSpriteName)
-            {
-                // Perform the desired action
-                PerformAction();
-            }
+            // Perform the desired action
+            PerformAction();
         }
     }
 
     private void PerformAction()
     {
+        Debug.Log("Correct object detected!");
         // Example action: Activate a GameObject
         if (actionObject != null)
         {
             actionObject.SetActive(true);
+            Debug.Log("Activated " + actionObject.name);
         }
 
         // You can add other actions here, such as playing a sound, changing the scene, etc.
